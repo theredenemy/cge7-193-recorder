c16c31ba92939234bbad09597c3e31b14bdb799e
theredenemy <1234corbin.1234ad@gmail.com>
2025-08-30 22:03:22 -0700

    modified:   main.py

--- main.py
+++ main.py
@@ -183,10 +183,12 @@ while (endloop3 < 1):
                         time.sleep(2)
                         source_functions.run_cmd("echo in-server")
                         if listfindlib.findtext(conlist, "in-server") == False:
+                            print("\nDisconnect")
                             source_functions.set_focus(process_name)
                             time.sleep(2)
                             pydirectinput.press("enter")
                             pydirectinput.press("enter")
+                            source_functions.run_cmd("echo 1; echo 2; echo 3; echo 4")
                             source_functions.move_demos(gamedir, demosdirname)
                             inserver = 0
                             print("RESET GAME")
@@ -222,10 +224,12 @@ while (endloop3 < 1):
                         time.sleep(2)
                         source_functions.run_cmd("echo in-server")
                         if listfindlib.findtext(conlist, "in-server") == False:
+                            print("\nDisconnect")
                             source_functions.set_focus(process_name)
                             time.sleep(2)
                             pydirectinput.press("enter")
                             pydirectinput.press("enter")
+                            # This is a Fix for an Endless Loop What the fuck
                             source_functions.run_cmd("echo 1; echo 2; echo 3; echo 4")
                             source_functions.move_demos(gamedir, demosdirname)
                             inserver = 0
@@ -235,6 +239,7 @@ while (endloop3 < 1):
                             # what
                             pydirectinput.press('esc')
                     if "Server connection timed out" in conlist:
+                        print("\nDisconnect")
                         print("FUCK")
                         source_functions.set_focus(process_name)
                         time.sleep(3)
