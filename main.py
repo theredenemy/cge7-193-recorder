0b3191d8264a40176e13a726bf5c4284b703e2d3
theredenemy <1234corbin.1234ad@gmail.com>
2025-09-02 17:05:48 -0700

    modified:   main.py

--- main.py
+++ main.py
@@ -241,7 +241,8 @@ while (endloop3 < 1):
                     
                     if listfindlib.findtext(conlist, "Disconnect") == True:
                         time.sleep(2)
-                        source_functions.run_cmd("echo in-server")
+                        for i in range(5):
+                            source_functions.run_cmd("echo in-server")
                         if listfindlib.findtext(conlist, "in-server") == False:
                             print("\nDisconnect")
                             source_functions.set_focus(process_name)
