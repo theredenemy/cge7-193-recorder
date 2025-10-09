419eb45468b86334c87c922d6a7f457ed146f517
theredenemy <1234corbin.1234ad@gmail.com>
2025-10-08 21:32:07 -0700

    modified:   main.py 	new file:   reload_config.py

---
+++ reload_config.py
@@ -0,0 +1,16 @@
+import configHelper
+from config_defaults import *
+from __main__ import configfile
+
+
+def reload_config():
+    import __main__
+    __main__.gamedir = configHelper.read_config(configfile, "SOURCETV", "gamedir", gamedir_default)
+    __main__.logfilename = configHelper.read_config(configfile, "SOURCETV", "logfilename", logfilename_default)
+    __main__.serverip = configHelper.read_config(configfile, "SOURCETV", "serverip", serverip_default)
+    __main__.serverport = configHelper.read_config(configfile, "SOURCETV", "serverport", serverport_default, True)
+    __main__.demosdirname = configHelper.read_config(configfile, "SOURCETV", "demosdirname", demosdirname_default)
+    __main__.appid = configHelper.read_config(configfile, "SOURCETV", "appid", appid_default)
+    __main__.process_name = configHelper.read_config(configfile, "SOURCETV", "process_name", process_name_default)
+    __main__.server_version = configHelper.read_config(configfile, "SOURCETV", "server_version", server_version_default)
+    __main__.uptime_days = configHelper.read_config(configfile, "SOURCETV", "uptime_days", uptime_days_default, True)
